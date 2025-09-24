cbe54b26ea4ab0507c80e32d63c93edd052d447e
awais khan <awaisalishah02@gmail.com>
2025-09-24 14:31:20 +0500

    Generation addition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
+import subprocess, sys
+
+try:
+    from rich.console import Console
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "rich==13.7.1"])
+    from rich.console import Console
+
+
 from app.ui.dashboard import Dashboard
 
 
 def main() -> None:
-	Dashboard.default().render()
+    Dashboard.default().render()
 
 
 if __name__ == "__main__":
-	main() 
+    main()
